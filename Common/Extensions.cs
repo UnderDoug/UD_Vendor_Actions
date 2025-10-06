d6206a85d670c3fe04b0649ab03341e3ea97d1dd
UnderDoug <douglas.marken@gmail.com>
2025-10-06 14:18:49 +1000

    Unified mroe of the code repeated by different tinkering services.
    Fixed up more grammar issues.
    
    Unified more of the game text across the different tinkering services.

---
+++ Common/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using XRL;
+using XRL.World;
+using XRL.World.Parts;
+using XRL.World.Tinkering;
+
+using UD_Modding_Toolbox;
+
+namespace UD_Vendor_Actions
+{
+    public static class Extensions
+    {
+        public static bool CanAfford(this GameObject Shopper, int DramsCost)
+        {
+            return !(Shopper.GetFreeDrams() < DramsCost);
+        }
+        public static bool CanAfford(this GameObject Shopper, double DramsCost)
+        {
+            return CanAfford(Shopper, (int)Math.Ceiling(DramsCost));
+        }
+    }
+}
